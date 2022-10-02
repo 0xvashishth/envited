7a22b27a9a73c9f57dd8815f2150082eafc950c6
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-10-02 19:49:49 +0530

    added form for submit event

--- src/components/CreateEvent/CreateEvent.js
+++ src/components/CreateEvent/CreateEvent.js
@@ -11,24 +11,31 @@ const CreateEvent = (props) => {
 
             <div className="row">
 
-                <div className="col-7">
+                <div className="col-4">
                     <div className="EventPagetext"> Create Your Event </div>
-                    <div className="input1">
-                        <div className="input1text">
-                        🎉 My event is called
-                        </div>
-                        <div className="input1input">
-                            <input type="text"/>
-                        </div>
-
-                    </div>
                 </div>
-
-                <div className="col-5">
-                <a href="/event" className="buttonright">
-                                <div className="buttoncontent">🎉 Submit event</div>
+            </div>
+            <div className="row">    
+                <div className="col-7">
+                <form>
+                <input type="text" name="name" class="question" id="nme" required autocomplete="off" />
+                <label for="nme"><span>🎉 My event is called</span></label><br/>
+                <input type="text" name="name" class="question" id="startdate" required autocomplete="off" />
+                <label for="startdate"><span>🗓 It starts at</span></label><br/>
+                <input type="text" name="name" class="question" id="enddate" required autocomplete="off" />
+                <label for="enddate"><span>🏁 It ends at (optional)</span></label><br/>
+                <input type="text" name="location" class="question" id="enddate" required autocomplete="off" />
+                <label for="location"><span>📍 It’s happening at (optional)</span></label><br/>
+                <input type="text" name="name" class="question" id="url" required autocomplete="off" />
+                <label for="url"><span>🔗 Add a URL link (optional)</span></label><br/>
+                <input type="text" name="name" class="question" id="description" required autocomplete="off" />
+                <label for="description"><span>✏️ Description (optional)</span></label>
+                </form><br/><br/>
+                <a href="/create" className="buttonright">
+                                <div className="buttoncontent">🎉 Create my event</div>
                             </a>
-                            
+                </div>
+                <div className="col-5">
                 </div>
 
             </div>
